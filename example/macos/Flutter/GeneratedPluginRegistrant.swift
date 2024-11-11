8616034d63395d99c5eb40c2d9fcce61783a0d0a
tkadziolka <tmax0135@gmail.com>
2024-11-11 19:55:32 +0100

    Updated dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import highlights_plugin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  HighlightsPlugin.register(with: registry.registrar(forPlugin: "HighlightsPlugin"))
 }
