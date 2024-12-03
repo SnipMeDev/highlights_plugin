c36e0e8d365242b5dc3034503c0866a02e688f59
tkadziolka <tmax0135@gmail.com>
2024-12-03 10:33:43 +0100

    fix: unused import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:highlights_plugin/highlights_plugin.dart';
 import 'package:highlights_plugin/model/phrase_location.dart';
 
